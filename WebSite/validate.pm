3274f263135067453c17c36bbce2ff3bf2d5b644
Alex Efros <powerman@powerman.name>
2016-05-21 07:21:57 +0300

    update for perl-5.24.0

--- WebSite/validate.pm
+++ WebSite/validate.pm
@@ -2,7 +2,6 @@ package WebSite::validate;
 use Mojo::Base -strict;
 use Validate::Tiny 1.501 qw( :all );
 use Export::Attrs;
-no warnings qw( experimental::lexical_topic ); ## no critic (ProhibitNoWarnings)   # my $_;
 
 
 # Validate rules for models
@@ -27,9 +26,9 @@ sub is_datetime :Export {
     state $DATE = qr/(20\d\d)  -  (0[1-9]|1[012])  -  (0[1-9]|[12]\d|3[01])/xms;
     state $TIME = qr/([01]\d|2[0-3])  :  ([0-5]\d)  :  ([0-5]\d)/xms;
     return sub {
-        my ($_) = @_;
-        return if !defined || !length;
-        return 'Bad datetime format' if !/\A$DATE $TIME\z/mso;
+        my ($v) = @_;
+        return if !defined $v || !length $v;
+        return 'Bad datetime format' if $v !~ /\A$DATE $TIME\z/mso;
         return 'Bad datetime value'  if !eval{timelocal($6,$5,$4,$3,$2-1,$1)};  ## no critic (ProhibitCaptureWithoutTest)
         return;
     };
@@ -38,44 +37,44 @@ sub is_datetime :Export {
 sub is_gt :Export {
     my ($f) = @_;
     return sub {
-        my ($_, $p) = @_;
-        return if !defined || !length;
-        return "Must be greater than '$p->{$f}'" if $_ le $p->{$f};
+        my ($v, $p) = @_;
+        return if !defined $v || !length $v;
+        return "Must be greater than '$p->{$f}'" if $v le $p->{$f};
         return;
     }
 }
 
 sub is_not_null :Export {
     return sub {
-        my ($_, $p, $f) = @_;
-        return 'Is null' if exists $p->{$f} && !defined;
+        my ($v, $p, $f) = @_;
+        return 'Is null' if exists $p->{$f} && !defined $v;
         return;
     };
 }
 
 sub is_scalar :Export {
     return sub {
-        my ($_) = @_;
-        return if !defined;
-        return 'Not a scalar' if ref;
+        my ($v) = @_;
+        return if !defined $v;
+        return 'Not a scalar' if ref $v;
         return;
     };
 }
 
 sub is_array :Export {
     return sub {
-        my ($_) = @_;
-        return if !defined;
-        return 'Not an array' if ref $_ ne 'ARRAY';
+        my ($v) = @_;
+        return if !defined $v;
+        return 'Not an array' if ref $v ne 'ARRAY';
         return;
     };
 }
 
 sub is_hash :Export {
     return sub {
-        my ($_) = @_;
-        return if !defined;
-        return 'Not a hash' if ref $_ ne 'HASH';
+        my ($v) = @_;
+        return if !defined $v;
+        return 'Not a hash' if ref $v ne 'HASH';
         return;
     };
 }
